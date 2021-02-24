bc4fbc0caff22164e4688c9c8f1a8b2e01163260
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-02-24 14:55:28 +0100

    Updated package and added missing java docs.

--- src/main/java/dev/thelecrafter/libraries/klooned/ColoredStringBuilder.java
+++ src/main/java/dev/thelecrafter/libraries/klooned/ColoredStringBuilder.java
@@ -5,27 +5,58 @@ import net.md_5.bungee.api.ChatColor;
 public class ColoredStringBuilder {
 
     private final String original;
-    private ChatColor[] pattern;
+    private ChatColor[] pattern = new ChatColor[]{};
 
+    /**
+     * A builder for colorized strings.
+     * @param original The original string you want to colorize with the builder.
+     */
     public ColoredStringBuilder(String original) {
         this.original = original;
     }
 
-    public void setPattern(ChatColor[] pattern) {
+    /**
+     * Set the pattern of the string (only net.md_5.bungee.api.ChatColor).
+     * @param pattern The pattern of the builder.
+     */
+    public ColoredStringBuilder setPattern(ChatColor[] pattern) {
         this.pattern = pattern;
+        return this;
     }
 
-    public void setPattern(ChatColor pattern) {
+    /**
+     * Set the pattern of the string (only net.md_5.bungee.api.ChatColor).
+     * @param pattern The pattern of the builder.
+     */
+    public ColoredStringBuilder setPattern(ChatColor pattern) {
         this.pattern = new ChatColor[]{pattern};
+        return this;
     }
 
+    /**
+     * Get the pattern of the string. Returns an empty array if no pattern was given.
+     * @return Returns the pattern of the string.
+     */
     public ChatColor[] getPattern() {
         return pattern;
     }
 
-    public String colorize(boolean isRainbow) {
-        if (isRainbow) return StringUtil.rainbow(original);
-        else return StringUtil.colorizeWithPattern(original, pattern);
+    /**
+     * Colorize the string.
+     * @return Returns the colorized string.
+     */
+    public String colorize() {
+        return StringUtil.colorizeWithPattern(original, pattern);
+    }
+
+    /**
+     * Colorize the string with the rainbow pattern.
+     * @param isExtendedRainbow The extended rainbow pattern is more detailed.
+     * @return Returns the colorized string.
+     */
+    public String colorizeRainbow(boolean isExtendedRainbow) {
+        if (isExtendedRainbow) return StringUtil.extendedRainbow(original);
+        else return StringUtil.rainbow(original);
     }
 
 }
