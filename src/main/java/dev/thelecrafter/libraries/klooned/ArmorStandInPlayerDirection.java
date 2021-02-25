1786c1cb7ef757217515a0e8a046f2271a7ba713
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-02-25 11:10:39 +0100

    New armor stand utility: ArmorStandInPlayerDirection.java. For more information visit the java docs.

--- src/main/java/dev/thelecrafter/libraries/klooned/ArmorStandInPlayerDirection.java
+++ src/main/java/dev/thelecrafter/libraries/klooned/ArmorStandInPlayerDirection.java
@@ -1,42 +1,40 @@
 package dev.thelecrafter.libraries.klooned;
 
-import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class FollowingArmorstands {
+public class ArmorStandInPlayerDirection {
 
     /**
      * A map of all currently active armor stands.
      */
     private static final Map<Player, ArmorStand> ARMOR_STAND_MAP = new HashMap<>();
     /**
-     * A set of all currently active tasks that control the player following armor stands.
+     * A set of all currently active tasks that control the armor stands in front of the player.
      */
     private static final Set<BukkitTask> TASKS = new HashSet<>();
 
     /**
-     * Let an armor stand follow a given player.
+     * Let an armor stand follow the player in front of the direction of the player.
      *
      * @param  player The player the armor stand will follow.
-     * @param  stand The armor stand that will follow the player.
+     * @param  stand The stand that will follow the player.
      * @param  plugin The plugin that is used to schedule tasks.
-     * @param  radius If the armor stand is outside of the radius, it will follow the player until its back in.
+     * @param  offset The offset of the player and the armor stand.
      * @return Returns if the method was successfully executed.
      */
-    public static boolean addFollowingArmorStand(Player player, ArmorStand stand, Plugin plugin, int radius) {
+    public static boolean addArmorStandInPlayerDirection(Player player, ArmorStand stand, Plugin plugin, int offset) {
         if (stand.isDead()) return false;
         ARMOR_STAND_MAP.put(player, stand);
-        BukkitTask task = new BukkitRunnable() {
+        BukkitTask task = new BukkitRunnable(){
             @Override
             public void run() {
                 if (stand.isDead() || !player.isOnline()) {
@@ -50,12 +48,7 @@ public class FollowingArmorstands {
                     this.cancel();
                     return;
                 }
-                if (player.getLocation().distance(stand.getLocation()) > radius) {
-                    Vector direction = player.getLocation().subtract(stand.getLocation()).clone().toVector();
-                    Location location = stand.getLocation().add(direction.clone().multiply(0.1));
-                    location.setDirection(direction);
-                    stand.teleport(location);
-                }
+                stand.teleport(player.getLocation().add(player.getLocation().getDirection().clone().multiply(offset)));
             }
         }.runTaskTimer(plugin, 0, 1);
         TASKS.add(task);
@@ -63,12 +56,12 @@ public class FollowingArmorstands {
     }
 
     /**
-     * Remove an armor stand from following a player. The armor stand will not be deleted.
+     * Remove an armor stand from teleporting in front of the player.
      *
      * @param  player The player the armor stand is currently following.
      * @return Returns if the method was successfully executed.
      */
-    public static boolean removeFollowingArmorStand(Player player) {
+    public static boolean removeArmorStandInPlayerDirection(Player player) {
         if (ARMOR_STAND_MAP.get(player) == null) return false;
         return ARMOR_STAND_MAP.remove(player, ARMOR_STAND_MAP.get(player));
     }
